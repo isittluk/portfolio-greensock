14c0b6397f67fcfce99554f65a71ae09d20ee65b
Luke J Isitt <luke_isitt@hotmail.com>
2021-12-24 13:32:36 -0800

    cleaned code, updated home
    added new images to home page, edited home page formatting, changed typography sizes, cleaned out scss and js files

--- js/main.js
+++ js/main.js
@@ -1,23 +1,3 @@
-console.log("hello");
-let tl = gsap.timeline({repeat:0});
-
-tl.to('.title-stagger', {
-    opacity:1,
-    transform: 'translateY(0px)', 
-    duration: .75, 
-    stagger: .333,
-    ease: "power3.out"
-});
-
-tl.to('.h2-color', {
-    delay: .75,
-    opacity: 1,
-    transform: 'translateY(0px)',
-    color: "#1B5DF9",
-    duration: .666,
-    ease: "power3.out"
-});
-
 /** cursor animation */
 let cursor = $(".cursor");
 follower = $(".cursor-follower");
@@ -54,37 +34,4 @@ $(document).on("mousemove", function(e) {
     mouseX = e.clientX;
     mouseY = e.clientY;
 });
-// yellow circle
-$(".link").on("mouseenter", function() {
-    cursor.addClass("active");
-    follower.addClass("active");
-});
-$(".link").on("mouseleave", function() {
-    cursor.removeClass("active");
-    follower.removeClass("active");
-});
-
-
-
-// scroll to fade in
-const containers = gsap.utils.toArray('.cs-container')
-  
-  containers.forEach((containers, i) => {
-      
-      gsap.timeline({
-      scrollTrigger: {
-        trigger: containers,
-        start: 'top bottom',
-        pinnedContainer: '#et-main-area',
-        markers: true,
-        toggleActions: "play complete complete reset",
-        fastScrollEnd: true,
-      },
-      // defaults: {ease: "none"}
-    }).from(containers, {duration: 1, autoAlpha: 0, y: 50})
-  })
-
-
-  /////////////////////////////////////////
-  var timeline = gsap.timeline();
 
