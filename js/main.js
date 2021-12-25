eaf48e0c3aa56c7abb6a1ec5802a2bac3c450d2b
Luke J Isitt <luke_isitt@hotmail.com>
2021-12-24 17:34:22 -0800

    added gsap scrollTrigger
    applied scrollTrigger to nav, started to optimize image and reduce page lag
    
    // issue is do to background properties in cs-image class

--- js/main.js
+++ js/main.js
@@ -1,37 +1,47 @@
 /** cursor animation */
-let cursor = $(".cursor");
-follower = $(".cursor-follower");
+// let cursor = $(".cursor");
+// follower = $(".cursor-follower");
 
-var posX = 0,
-    posY = 0;
+// var posX = 0,
+//     posY = 0;
 
-var mouseX = 0,
-    mouseY = 0;
+// var mouseX = 0,
+//     mouseY = 0;
 
-TweenMax.to({}, 0.016, {
-  repeat: -1,
-  onRepeat: function() {
-    posX += (mouseX - posX) / 8;
-    posY += (mouseY - posY) / 8;
+// TweenMax.to({}, 0.016, {
+//   repeat: -1,
+//   onRepeat: function() {
+//     posX += (mouseX - posX) / 8;
+//     posY += (mouseY - posY) / 8;
 
-    TweenMax.set(follower, {
-        css: {
-        left: posX - 12,
-        top: posY - 12
-        }
-    });
+//     TweenMax.set(follower, {
+//         css: {
+//         left: posX - 12,
+//         top: posY - 12
+//         }
+//     });
 
-    TweenMax.set(cursor, {
-        css: {
-        left: mouseX,
-        top: mouseY
-        }
-    });
-  }
+//     TweenMax.set(cursor, {
+//         css: {
+//         left: mouseX,
+//         top: mouseY
+//         }
+//     });
+//   }
+// });
+
+// $(document).on("mousemove", function(e) {
+//     mouseX = e.clientX;
+//     mouseY = e.clientY;
+// });
+
+
+
+/////////////
+ScrollTrigger.create({
+    start: 'top -80',
+    end: 99999,
+    toggleClass: {className: 'nav-extended', targets: '.nav'}
 });
 
-$(document).on("mousemove", function(e) {
-    mouseX = e.clientX;
-    mouseY = e.clientY;
-});
-
+  
