0f1d512db78e40071787fb3a484ff17f0cbe9d54
Luke J Isitt <luke_isitt@hotmail.com>
2021-12-27 10:56:52 -0800

    created reavealUp anim
    added revealUp anim to elements on home

--- js/main.js
+++ js/main.js
@@ -37,11 +37,94 @@
 
 
 
-/////////////
-ScrollTrigger.create({
-    start: 'top -80',
-    end: 99999,
-    toggleClass: {className: 'nav-extended', targets: '.nav'}
+//// nav animation on scroll ////
+// ScrollTrigger.create({
+//     start: 'top -80',
+//     end: 99999,
+//     toggleClass: {className: 'nav-extended', targets: '.nav'}
+// });
+
+//// hero h1 anim on load ////
+// gsap.from(".anim-revealUp", {
+//     delay: 0.5,
+//     opacity: 0,
+//     y: 50,
+//     ease: "power2",
+//     duration: 1,
+// });
+
+
+
+// // test
+// gsap.from(".anim-onLoad", {
+//     delay: 0.5,
+//     opacity: 0,
+//     y: 50
+// });
+
+let tl = gsap.timeline();
+const revealUp = gsap.utils.toArray(".anim-revealUp");
+revealUp.forEach(anim => {
+    gsap.from(anim, {
+        opacity: 0,
+        y: 50,
+        ease: "power2",
+        duration: 0.75,
+        scrollTrigger: {
+            trigger: anim,
+            start: "top top+=300",
+            end: "top top",
+            toggleActions: "play none none reverse",
+            markers: true
+        }
+    })
 });
+
+
+
+// tl.from(".anim-revealUp", {
+//     delay: 0.5,
+//     opacity: 0,
+//     y: 50,
+//     scrollTrigger: {
+//         trigger: "section",
+//         markers: true
+//     }
+// })
+
+gsap.to(".square", {
+    x:500,
+    opacity: 0,
+    scrollTrigger: {
+        trigger: ".square2",
+        start: "top bottom",
+        end: "top center",
+        scrub: true,
+
+}})
+
+
+
+// momentum scrolling
+// console.clear();
+
+// gsap.registerPlugin(ScrollTrigger);
+
+// var container = document.querySelector("#scroll-container");
+
+// var height = container.clientHeight;
+// document.body.style.height = height + "px";
+
+// gsap.to(container, {
+//   y: -(height - document.documentElement.clientHeight),
+//   scrollTrigger: {
+//     trigger: document.body,
+//     start: "top top",
+//     end: "bottom bottom",
+//     scrub: 1
+//   }
+// });
+
+
 
   
